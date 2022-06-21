9ff62c805fa27066651ae5cca6b5eacf61f3b06e
hyunsang-ahn <happylovetkd@naver.com>
2022-06-21 20:57:27 +0900

    react hook useMemo 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 // import Counter from './Components/Counter'
-import Info from './Components/Info'
+// import Info from './Components/Info'
+import Average from './Components/Average'
 
 function App() {
   const [visible, setVisible] = useState(false)
   return (
-    <div>
-      <button onClick={() => { setVisible(!visible) }} >{visible ? '숨기기' : '보이기'}</button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    <Average />
+    // <div>
+    //   <button onClick={() => { setVisible(!visible) }} >{visible ? '숨기기' : '보이기'}</button>
+    //   <hr />
+    //   {visible && <Info />}
+    // </div>
   );
 }
 
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,19 +1,29 @@
-import React,{ useState } from "react";
+import React, { useReducer } from "react";
+
+function reducer(state, action) {
+  //action.type에 따라서 다른작업을 수행한다.
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 }
+    case 'DECREMENT':
+      return { value: state.value - 1 }
+    default:
+      //아무것도 아닌상황
+      return state
+  }
+}
 
 const Counter = () => {
-  const [value, setValue] = useState(0)
-
-  return(
+  const [state, dispatch] = useReducer(reducer, { value : 0})
+  return (
     <div>
       <p>
-        현재 카운터 값은 <b>{value}</b> 입니다
+        현재 카운터 값은 <b>{state.value}</b>
       </p>
-      <button onClick={()=> setValue(value + 1)}>+</button>
-      <button onClick={()=> setValue(value -1)}>-</button>
+      <button onClick={()=> dispatch({type : 'INCREMENT'})}>+</button>
+      <button onClick={()=> dispatch({type : 'DECREMENT'})}>-</button>
 
     </div>
   )
 }
-
-
 export default Counter
